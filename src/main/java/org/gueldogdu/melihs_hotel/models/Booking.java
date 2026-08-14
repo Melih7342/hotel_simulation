88428e4f254372af6cfda309efd753412e40df0f
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-08-14 18:00:41 +0200

    added entity Booking

--- src/main/java/org/gueldogdu/melihs_hotel/models/Booking.java
+++ src/main/java/org/gueldogdu/melihs_hotel/models/Booking.java
@@ -1,7 +1,28 @@
 package org.gueldogdu.melihs_hotel.models;
 
-public class Booking {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.bytebuddy.asm.Advice;
 
-    private Long booking_id;
-    private Hotel hotel;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    private Long hotelId;
+    private Long roomId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestName;
 }
