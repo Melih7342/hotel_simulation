0160b96820c1999001982c74e72c8008e6a93107
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-08-15 23:03:51 +0200

    created the booking service with the first method

--- src/main/java/org/gueldogdu/melihs_hotel/exceptions/RoomAlreadyBookedException.java
+++ src/main/java/org/gueldogdu/melihs_hotel/exceptions/RoomAlreadyBookedException.java
@@ -1,7 +1,11 @@
 package org.gueldogdu.melihs_hotel.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.time.LocalDate;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class RoomAlreadyBookedException extends RuntimeException {
     public RoomAlreadyBookedException(Long roomId, LocalDate requestedCheckIn, LocalDate requestedCheckOut) {
         super(String.format("Room %d is already booked between %s and %s!", roomId, requestedCheckIn, requestedCheckOut));
--- src/main/java/org/gueldogdu/melihs_hotel/services/BookingService.java
+++ src/main/java/org/gueldogdu/melihs_hotel/services/BookingService.java
@@ -1,20 +1,37 @@
 package org.gueldogdu.melihs_hotel.services;
 
+import lombok.RequiredArgsConstructor;
+import org.gueldogdu.melihs_hotel.enums.BookingStatus;
+import org.gueldogdu.melihs_hotel.exceptions.RoomAlreadyBookedException;
 import org.gueldogdu.melihs_hotel.models.Booking;
 import org.gueldogdu.melihs_hotel.repositories.BookingRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class BookingService {
-    BookingRepository  bookingRepository;
+    private final BookingRepository  bookingRepository;
 
-    public BookingService(BookingRepository bookingRepo) {
-        this.bookingRepository = bookingRepo;
-    }
-
-    private void createBooking(Long hotelId, Long roomId, LocalDate requestedCheckIn, LocalDate requestedCheckOut) {
+    @Transactional
+    public Booking createBooking(Long hotelId, Long roomId, LocalDate requestedCheckIn, LocalDate requestedCheckOut) {
         List<Booking> overlappingBookings = bookingRepository.findOverlappingBookings(roomId, requestedCheckIn, requestedCheckOut);
 
+        if (!overlappingBookings.isEmpty()) {
+            throw new RoomAlreadyBookedException(roomId, requestedCheckIn, requestedCheckOut);
+        }
+        Booking booking = new Booking();
+        booking.setHotelId(hotelId);
+        booking.setRoomId(roomId);
+        booking.setCheckInDate(requestedCheckIn);
+        booking.setCheckOutDate(requestedCheckOut);
+        booking.setStatus(BookingStatus.CONFIRMED);
+        bookingRepository.save(booking);
+
+        return booking;
+        }
     }
 }
