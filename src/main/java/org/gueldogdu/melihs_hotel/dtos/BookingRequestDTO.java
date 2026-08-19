b20fe4efdeb0d623147c234324d52ca5db42a489
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-08-20 00:41:13 +0200

    added logic to dto

--- src/main/java/org/gueldogdu/melihs_hotel/dtos/BookingRequestDTO.java
+++ src/main/java/org/gueldogdu/melihs_hotel/dtos/BookingRequestDTO.java
@@ -1,5 +1,6 @@
 package org.gueldogdu.melihs_hotel.dtos;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
@@ -16,15 +17,22 @@ public class BookingRequestDTO {
     @NotNull(message = "A room-ID must be provided")
     private Long roomId;
 
-    @NotNull(message = "A checkin date must be provided")
-    @FutureOrPresent(message = "The checkin date can't be in the past")
+    @NotNull(message = "A check in date must be provided")
+    @FutureOrPresent(message = "The check in date can't be in the past")
     private LocalDate checkInDate;
 
-    @NotNull(message = "A checkout date must be provided")
-    @Future(message = "The checkout date must be in the future")
+    @NotNull(message = "A check out date must be provided")
+    @Future(message = "The check out date must be in the future")
     private LocalDate checkOutDate;
 
     @NotNull(message = "Please provide your name")
     private String guestName;
 
+    @AssertTrue(message = "The check in date must be before the check out date")
+    public boolean isCheckOutAfterCheckIn() {
+        if (checkInDate == null || checkOutDate == null) {
+            return true;
+        }
+        return checkOutDate.isAfter(checkInDate);
+    }
 }
