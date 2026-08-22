2420effa97359afceee30e67cd01f832d7a9ef0a
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-08-22 19:01:10 +0200

    created Persona struct

--- simulation/Persona.go
+++ simulation/Persona.go
@@ -1,6 +1,11 @@
 package main
 
 type Persona struct {
-	Name string `json:"name"`
-
+	Id                string  `json:"id"`
+	Name              string  `json:"name"`
+	Weight            int     `json:"weight"`
+	MinStayDays       int     `json:"minStayDays"`
+	MaxStayDays       int     `json:"maxStayDays"`
+	PreferredHotels   []int   `json:"preferredHotels"`
+	CancelProbability float64 `json:"cancelProbability"`
 }
